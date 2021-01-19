68e08f7a846a323b4f441e48ef30061aba2ef595
unknown <s4929892@bournemouth.ac.uk>
2021-01-19 22:15:18 +0000

    creatures behavious part 1

---
+++ Assets/Scripts/AI/Bot.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum TypeOfCreature
+{
+   brain, bubble, poop 
+}
+public class Bot : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+    [SerializeField] TypeOfCreature tc;
+    CreaturesMaster cm;
+    GameObject lastTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tc = TypeOfCreature.poop;
+        agent = this.GetComponent<NavMeshAgent>();
+        cm = FindObjectOfType<CreaturesMaster>();
+
+        FindTargetTochase();
+
+    }
+
+    internal void FindTargetTochase()
+    {
+        if (tc == TypeOfCreature.poop)
+        {
+            target = cm.GetPoopRandom(this);
+            lastTarget = target;
+
+        }
+    }
+
+    public void Seek(Vector3 location)
+    {
+        agent.SetDestination(location);
+    }
+
+    public void Flee(Vector3 location)
+    {
+        Vector3 fleeVector = location - this.transform.position;
+        agent.SetDestination(this.transform.position - fleeVector);
+    }
+
+    public void Pursue()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+
+        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
+
+        if((toTarget>90&&relativeHeading<20)|| target.GetComponent<Drive>().currentSpeed<0.01f)
+        {
+            Seek(target.transform.position);
+            return;
+        }
+
+        float lookAhed = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        Seek(target.transform.position + target.transform.forward * lookAhed);
+    }
+
+    public void Evade()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float lookAhed = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        Flee(target.transform.position + target.transform.forward * lookAhed);
+    }
+
+
+    
+}
---
+++ Assets/Scripts/AI/CreaturesMaster.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CreaturesMaster : MonoBehaviour
+{
+    int brainrandom, pooprandom;
+    List<GameObject> targets=new List<GameObject>();
+    List<Bot> PoopMonsters, BrainMonsters;
+    GameObject Player;
+    bool runfromPlayer=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject targ in GameObject.FindGameObjectsWithTag("wp"))
+        {
+            targets.Add(targ);
+        }
+        Player = GameObject.FindGameObjectWithTag("Player");
+        BrainMonsters = new List<Bot>();
+        PoopMonsters = new List<Bot>();
+        brainrandom = getRandomTarget();
+        pooprandom = getRandomTarget();
+    }
+
+     int getRandomTarget()
+    {
+        int randomT;
+        return randomT = Random.Range(0, targets.Count);
+
+    }
+
+    public GameObject GetBrainRandom(Bot brainMonster)
+    {
+        if(!BrainMonsters.Contains(brainMonster))
+        {
+            BrainMonsters.Add(brainMonster);
+        }
+      
+        return targets[brainrandom];
+    }
+    public GameObject GetPoopRandom(Bot poopMonster)
+    {
+        if(!PoopMonsters.Contains(poopMonster))
+        {
+            PoopMonsters.Add(poopMonster);
+        }
+        return targets[pooprandom];
+    }
+    void Update()
+    {
+        foreach(Bot creature in PoopMonsters)
+        {
+            if (creature.target)
+            {
+                if(!runfromPlayer)
+                {
+                    if (Vector3.Distance(creature.transform.position, Player.transform.position) > 1 )
+                    {
+                        creature.agent.speed = 3.5f;
+                        creature.FindTargetTochase();
+                        creature.Seek(creature.target.transform.position);
+                    }
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        runfromPlayer = true;
+                    }
+                }
+                else
+                {
+                    if (Vector3.Distance(creature.transform.position, Player.transform.position) < 10)
+                    {
+                        creature.target = Player;
+                        creature.agent.speed = 7;
+                        creature.Evade();
+                    }
+                    else
+                    {
+                        brainrandom = getRandomTarget();
+                        pooprandom = getRandomTarget();
+                        runfromPlayer = false;
+                    }
+                    
+                }
+
+
+            }
+        }
+       
+
+    }
+}
